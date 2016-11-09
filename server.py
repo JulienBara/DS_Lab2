e57ce8c3885a6310e9a437e1830481ed25857221
JulienBara <bara.julien@yahoo.fr>
2016-11-09 15:13:05 +0000

    Refactor

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import socket
 import sys
 from thread import *
-from urlparse import urlparse, parse_qs
-
 
 if len(sys.argv) > 3:
     host = str(sys.argv[1])
@@ -13,11 +11,10 @@ else:
     port = 8015
     nbrCoAllowed = 10
 
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ("socket created")
 
-#Bind socket to local host and port
+# Bind socket to local host and port
 try:
     s.bind((host, port))
 except socket.error as msg:
@@ -26,16 +23,17 @@ except socket.error as msg:
 
 print ('Socket bind complete')
 
-#Start listening on socket
+# Start listening on socket
 s.listen(nbrCoAllowed)
 print ('Socket now listening')
 
-#Function for handling connections. This will be used to create threads
-def clientthread(conn):
-    #Sending message to connected client
-    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
 
-    #infinite loop so that function do not terminate and thread do not end.
+# Function for handling connections. This will be used to create threads
+def clientThread(conn):
+    # Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n')  # send only takes string
+
+    # infinite loop so that function do not terminate and thread do not end.
     while True:
 
         # #Receiving from client
@@ -44,15 +42,9 @@ def clientthread(conn):
         # if not data: 
         #     break
 
-        #conn.sendall(reply)
+        # conn.sendall(reply)
 
         data = conn.recv(4096)
-        # print urlparse(data)
-        # print "query : " + urlparse(data).query
-        # print "pars query : "
-        # print parse_qs(urlparse(data).query)
-        # print "pars : "
-        # print parse_qs(urlparse(data))
 
         if not data:
             break
@@ -63,16 +55,17 @@ def clientthread(conn):
         if data == "HELO text\n":
             conn.send("HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n")
 
-    #came out of loop
+    # came out of loop
     conn.close()
 
-#now keep talking with the client
+
+# now keep talking with the client
 while 1:
-    #wait to accept a connection - blocking call
+    # wait to accept a connection - blocking call
     conn, addr = s.accept()
     print ('Connected with ' + addr[0] + ':' + str(addr[1]))
 
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
+    # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientThread, (conn,))
 
 s.close()
