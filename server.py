1a681fbfde27116be6ae91c2e188530053f019ed
JulienBara <bara.julien@yahoo.fr>
2016-11-09 15:10:33 +0000

    Improve server

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+from thread import *
+from urlparse import urlparse, parse_qs
 
 
 if len(sys.argv) > 3:
@@ -8,7 +10,7 @@ if len(sys.argv) > 3:
     nbrCoAllowed = int(sys.argv[3])
 else:
     host = "localhost"
-    port = 8000
+    port = 8015
     nbrCoAllowed = 10
 
 
@@ -28,11 +30,49 @@ print ('Socket bind complete')
 s.listen(nbrCoAllowed)
 print ('Socket now listening')
 
- 
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+
+        # #Receiving from client
+        # data = conn.recv(1024)
+        # reply = 'OK...' + data
+        # if not data: 
+        #     break
+
+        #conn.sendall(reply)
+
+        data = conn.recv(4096)
+        # print urlparse(data)
+        # print "query : " + urlparse(data).query
+        # print "pars query : "
+        # print parse_qs(urlparse(data).query)
+        # print "pars : "
+        # print parse_qs(urlparse(data))
+
+        if not data:
+            break
+
+        if data == "KILL_SERVICE\n":
+            conn.send("Not implemented yet")
+
+        if data == "HELO text\n":
+            conn.send("HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n")
+
+    #came out of loop
+    conn.close()
+
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
     print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-     
+
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+
 s.close()
