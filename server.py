cbe24ad7a8f4568cf35d033b6bb5feb1bc6b41e7
JulienBara <bara.julien@yahoo.fr>
2016-11-10 15:36:17 +0000

    Change port

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 3:
     nbrCoAllowed = int(sys.argv[3])
 else:
     host = "localhost"
-    port = 9000
+    port = 3000
     nbrCoAllowed = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
